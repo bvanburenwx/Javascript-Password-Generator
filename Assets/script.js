1fb27257e789136b9864fead3141b71365e3f7cb
BVanBuren <bvanburenwx@gmail.com>
2021-06-20 20:26:25 -0400

    Adding changes to the style.js file. created functions to randomize the a number return, uppercase and lowercase letter return. Also added else if function to return an alert if number is chosen outside of 8-128 characters

--- Assets/script.js
+++ Assets/script.js
@@ -15,6 +15,27 @@ generateBtn.addEventListener("click", writePassword);
 
 // function to generate password
 function generatePassword() {
-  okKey = parseInt(prompt("How many characters would you like your password to be? Please choose between 8 and 128 characters"))
-  
+  enteredValue = parseInt(prompt("How many characters would you like your password to be? Please choose between 8 and 128 characters"))
+
+  if (!enteredValue) {
+    alert("You must select a value");
+  } else if (enteredValue < 8 || enteredValue > 128); {
+
+    enteredValue = parseInt(prompt("Your choice must be between 8 and 128"));
+  }
+
 }
+// creating a function using the numeric character code values to generate random lowercase letters
+function randomLowercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+// creating a function using the numeric character code values to generate random uppercase letters
+function randomUppercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+// creating a function using the numeric character code values to generate random numbers from 0-9
+function randomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+console.log(randomNumber());
