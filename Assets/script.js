53671e8e293f59850fd47b1068aa96b930c67f6c
BVanBuren <bvanburenwx@gmail.com>
2021-06-23 18:32:53 -0400

    fixed some errors in the code and moved the actualPassword variable from global to local variable to fix an issue.

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var actualPassword = ""
+
 var generateBtn = document.querySelector("#generate");
 var allChoices = "";
 var lowerCase = "qwertyuioplkjhgfdsazxcvbnm"
@@ -21,16 +21,17 @@ generateBtn.addEventListener("click", writePassword);
 
 // function to generate password
 function generatePassword() {
+var actualPassword = ""
 var enteredValue = parseInt(prompt("How many characters would you like your password to be? Please choose between 8 and 128 characters"))
 // if else statement used to prompt that a value must be between 8-128 characters. 
   if (!enteredValue) {
     alert("You must select a value");
-  } else if (enteredValue < 8 && enteredValue > 128) {
+  } else if (enteredValue < 8 || enteredValue > 128) {
 // prompt used to let user know that the character they choose must be between 8 and 128.
   enteredValue = parseInt(prompt("Your choice must be between 8 and 128"));
-  } else {
+  } 
     // using a confirm prompt to decide if user wants uppercase letters as a part of password
-    var capitalLetters = window.confirm("Would you like upppercase letters?")
+    var capitalLetters = window.confirm("Would you like uppercase letters?")
     
     // using a confirm prompt to decide if user wants lowercase letters as a part of password
     var lowerCaseLetters = window.confirm("Would you like lowercase letters?")
@@ -54,15 +55,15 @@ var enteredValue = parseInt(prompt("How many characters would you like your pass
     if (symbols === true) {
             allChoices += symbolChoices;
     }
-    
+
     // A for loop to make sure that once all of the statements are true they produce the right amount of characters.
     for (var i = 0; i < enteredValue; i++) {
-        var pass = Math.floor(Math.random() * allChoices.length + 1);
+        var pass = Math.floor(Math.random() * allChoices.length);
 
       // continues to concatinate random characters from the string until the desired number of characters is reached. 
         actualPassword += allChoices.charAt(pass);
+        console.log(actualPassword.length)
     }
-    return actualPassword;
-  } 
-
+    return actualPassword; 
+    
 }
